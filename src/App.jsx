6a5390cf2b93a1af08587fe3d1bcef0112bbed39
Moe <masihdon@hotmail.de>
2023-08-16 22:17:05 +0200

    add components and api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+const [activities, setActivities] = useState([])
 
+  async function fetchActivities() {
+  try{
+  const response = await fetch("http://www.boredapi.com/api/activity/");
+    const data = await response.json();
+    const activity = data.activity
+  setActivities([activity])
+  console.log(data);
+  } catch (error) {
+  console.error("Error fetching activities:", error);
+}
+}
+  useEffect(() => {
+    fetchActivities();
+  }, [])
+  
   return (
     <>
-      <h1>Bored</h1>
+      <button>Get random activity</button>
+      <h1>{ activities }</h1>
     </>
   )
 }
---
+++ src/components/DropDown.jsx
@@ -0,0 +1,9 @@
+function DropDown() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DropDown
