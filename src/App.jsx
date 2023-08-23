8276f0b6e3537775f203a45da0692954d09785dd
Moe <masihdon@hotmail.de>
2023-08-23 10:39:57 +0200

    save favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Button from './components/Button';
-import './App.css'
+import Heart from "react-animated-heart";
+import { v4 as uuidv4 } from 'uuid';
 import Categories from './components/Categories';
+import './App.css'
 
 function App() {
-  const [activities, setActivities] = useState([])
+  const [activities, setActivities] = useState({
+    activity: '',
+    participants: 0,
+    price: 0
+  })
   const [category, setCategory] = useState([])
   const [isClicked, setIsClicked] = useState(false);
-
+const [fav, setFav] = useState([])
+  const [isClick, setClick] = useState(false);
+  
+   const uniqueId = uuidv4(); 
 
   async function fetchActivities(selectedCategory) {
     try {
@@ -18,7 +27,11 @@ function App() {
       const response = await fetch(url);
       const data = await response.json();
       
-      setActivities(data.activity)
+      setActivities({
+      activity: data.activity,
+      participants: data.participants,
+      price: data.price
+    })
      
     } catch (error) {
       console.error("Error fetching activities:", error);
@@ -30,12 +43,32 @@ function App() {
   function handleButtonClick() {
     fetchActivities(category)
     setIsClicked(true)
+    setClick(false)
     console.log(category)
   }
 
+   function HandleSaveFav(activities) {
+  if (isClick) {
+    const newFavList = fav.filter(item => item.activity !== activities);
+    setFav(newFavList);
+  } else {
+    const newFav = { id: uniqueId, activity: activities };
+    setFav(currentFav => [...currentFav, newFav]);
+  }
+  
+  setClick(isClick => !isClick);
+}
+
+  
+   useEffect(() => {
+    console.log('Favorites updated:', fav);
+  }, [fav]);
+
   return (
   <>
-      <div className="top-nav"><a href="" target="_blank">about bored</a></div>
+      <div className="top-nav">
+        <a href="" target="_blank">about bored</a>
+      </div>
       <div className="header">
         <h1>Bored?</h1>
         <h2>Casual & Daily Activities</h2>
@@ -43,9 +76,17 @@ function App() {
      </div>
 
       <Categories onSetCategory={setCategory}/>
-      <h1>{activities}</h1>
+      <h1>{activities.activity}</h1>
+      {isClicked &&
+        <Heart isClick={isClick} onClick={() => HandleSaveFav(activities)} />}
+    <p>Participants: {activities.participants}</p>
+    <p>Price: {activities.price}</p>
+     
+     
+      
+
       <h2>{category}</h2>
-      {/* Passing down fetchActivities function as props to Button component */}
+      
       <Button
         OnHandleButtonClick={handleButtonClick}
         category={category}
