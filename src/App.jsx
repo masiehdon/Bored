a041970bb890a5c8555772777d13917b0beb65e9
belloshe <bellabello199@hotmail.com>
2023-08-25 16:28:39 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Navbar from './components/Navbar';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
