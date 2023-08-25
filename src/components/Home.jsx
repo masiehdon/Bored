07db8a50f2be8a05000b79f6292482c1ce1a589e
PernillaTheStudent <pernilla.codex@gmail.com>
2023-08-25 10:57:41 +0200

    Add content activity to Masiehs fetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,88 +5,88 @@ import Categories from './Categories';
 import Favorites from './Favorites';
 import '../App.css'
 
+import Activity from './Activity';
+
 function Home() {
-  const [activities, setActivities] = useState({
-    activity: '',
-    participants: 0,
-    price: 0
-  });
-  const [category, setCategory] = useState('');
-  const [isClicked, setIsClicked] = useState(false);
-  const [fav, setFav] = useState([]);
-  const [saved, setSaved] = useState(false);
+	const [activities, setActivities] = useState({
+		activity: '',
+		participants: 0,
+		price: 0
+	});
+	const [category, setCategory] = useState('');
+	const [isClicked, setIsClicked] = useState(false);
+	const [fav, setFav] = useState([]);
+	const [saved, setSaved] = useState(false);
 
-  const uniqueId = useId();
+	const uniqueId = useId();
 
-  async function fetchActivities(selectedCategory) {
-    try {
-      const url = selectedCategory
-        ? `https://www.boredapi.com/api/activity?type=${selectedCategory}`
-        : `https://www.boredapi.com/api/activity/`
+	const [suggestion, setSuggestion] = useState(null);
+	const [categorySuggestion, setCategorySuggestion] = useState("")
+	const [buttonClicked, setButtonClicked] = useState(false);
+	const activityTypes = [
+		{ type: '', label: 'Random' },
+		{ type: 'relaxation', label: 'Relax' },
+		{ type: 'social', label: 'Social' },
+		{ type: 'cooking', label: 'Cooking' },
+		{ type: 'education', label: 'Learning' },
+	];
 
-      const response = await fetch(url);
-      const data = await response.json();
+	async function fetchActivities(selectedCategory) {
+		try {
+			const url = selectedCategory
+				? `https://www.boredapi.com/api/activity?type=${selectedCategory}`
+				: `https://www.boredapi.com/api/activity/`
 
-      setActivities({
-        activity: data.activity,
-        participants: data.participants,
-        price: data.price,
-        id: uniqueId
-      });
+			const response = await fetch(url);
+			const data = await response.json();
 
-    } catch (error) {
-      console.error("Error fetching activities:", error);
-    }
-  }
+			setSuggestion(data);   // tillägg
 
-  function handleButtonClick() {
-    fetchActivities(category);
-    setIsClicked(true);
-    setSaved(false);
-  }
+			setActivities({
+				activity: data.activity,
+				participants: data.participants,
+				price: data.price,
+				id: uniqueId
+			});
 
-  function handleSaveFav() {
-    if (saved) {
-      const newFavList = fav.filter(item => item.id !== activities.id);
-      setFav(newFavList);
-    } else if (!fav.some(item => item.id === activities.id)) {
-      const newFav = { id: activities.id, activity: activities.activity };
-      setFav(currentFav => [...currentFav, newFav]);
-    }
-    setSaved(curr => !curr);
-  }
+		} catch (error) {
+			console.error("Error fetching activities:", error);
+		}
+	}
+	
+	function handleButtonClick(category) {
+		setCategorySuggestion(category);
+		setButtonClicked(true);
+		fetchActivities(category);
+		setIsClicked(true);
+		setSaved(false);
+	}
 
-  const shouldDisplayDetails = activities.activity !== '';
+	return (
+		<>
 
-  return (
-    <>
-      <div className="header">
-        <h1>Bored?</h1>
-        <h2>Casual & Daily Activities</h2>
-        <p className="ingress">Get ideas for activities. Click on a button.</p>
-      </div>
+			<div className={`header ${buttonClicked ? 'move-up' : ''}`}>
+				<h1>Bored?</h1>
+				<h2>Casual & Daily Activities</h2>
+				<p className="ingress">Get ideas for activites. Click on a button.</p>
+			</div>
 
-      <Categories onSetCategory={setCategory} onHandleButtonClick={handleButtonClick} />
+			<div className="activity-menu">
+				{activityTypes.map(({ type, label }) => (
+					<button
+						key={type}
+						className={`activity ${type}`}
+						onClick={() => handleButtonClick(type)}
+					>
+						{label}
+					</button>))};
+			</div>
 
-      {shouldDisplayDetails && (
-        <>
-          <h1>{activities.activity}</h1>
-          <button onClick={handleSaveFav}>{saved ? 'Saved!' : 'Save'}</button>
-          <h3>Participants: {activities.participants}</h3>
-          <h3>Price: {activities.price}</h3>
-        </>
-      )}
+			{buttonClicked &&
+				<Activity category={categorySuggestion} {...suggestion} />}
 
-      <Button
-        OnHandleButtonClick={handleButtonClick}
-        category={category}
-      >
-        {isClicked ? "Next" : "Feeling Bored Again?"}
-      </Button>
-
-      <Favorites fav={fav} />
-    </>
-  );
+		</>
+	);
 }
 
 export default Home;
